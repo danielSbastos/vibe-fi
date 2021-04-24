a48aa73c676f173f6d431fae6fbbc1b97326da59
Leonardo Gomide <leocgomide@gmail.com>
2021-04-24 14:44:25 -0300

    Melhorias no script de criação

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS vibeFi;
 
-DROP TABLE IF EXISTS vibeFi.Usuario ;
+DROP TABLE IF EXISTS vibeFi.vibeSeed;
+DROP TABLE IF EXISTS vibeFi.VibeUsuario;
+DROP TABLE IF EXISTS vibeFi.VibeBase;
+DROP TABLE IF EXISTS vibeFi.Usuario;
 
 CREATE TABLE IF NOT EXISTS vibeFi.Usuario (
-  idSpotify CHAR(22) NOT NULL,
+  idSpotify CHAR(22) UNIQUE NOT NULL,
   Nome VARCHAR(80) NOT NULL,
   grauPopular INT NULL,
   grauFeliz DOUBLE PRECISION NULL,
@@ -11,15 +14,12 @@ CREATE TABLE IF NOT EXISTS vibeFi.Usuario (
   grauAcustico DOUBLE PRECISION NULL,
   grauDancavel DOUBLE PRECISION NULL,
   PRIMARY KEY (idSpotify));
-
 CREATE UNIQUE INDEX idSpotifyUser_UNIQUE ON vibeFi.Usuario (idSpotify ASC);
 
 
-DROP TABLE IF EXISTS vibeFi.VibeBase ;
-
 CREATE TABLE IF NOT EXISTS vibeFi.VibeBase (
-  idVibe CHAR(22) NOT NULL,
-  Nome VARCHAR(80) NOT NULL,
+  idVibe CHAR(22) UNIQUE NOT NULL,
+  nome VARCHAR(80) NOT NULL,
   descricao VARCHAR(240) NULL,
   grauPopular INT NULL,
   grauFeliz DOUBLE PRECISION NULL,
@@ -27,18 +27,15 @@ CREATE TABLE IF NOT EXISTS vibeFi.VibeBase (
   grauAcustico DOUBLE PRECISION NULL,
   grauDancavel DOUBLE PRECISION NULL,
   PRIMARY KEY (idVibe));
-
 CREATE UNIQUE INDEX idVibeBase_UNIQUE ON vibeFi.VibeBase (idVibe ASC);
 
 
-DROP TABLE IF EXISTS vibeFi.VibeUsuario ;
-
 CREATE TABLE IF NOT EXISTS vibeFi.VibeUsuario (
-  idVibe CHAR(22) NOT NULL,
+  idVibe CHAR(22) UNIQUE NOT NULL,
   idUsuario CHAR(22) NOT NULL,
-  Nome VARCHAR(80) NOT NULL,
+  nome VARCHAR(80) NOT NULL,
   descricao VARCHAR(240) NULL,
-  Origem CHAR(22) NULL,
+  origem CHAR(22) NULL,
   grauPopular INT NULL,
   grauFeliz DOUBLE PRECISION NULL,
   bpmMedio FLOAT NULL,
@@ -46,26 +43,18 @@ CREATE TABLE IF NOT EXISTS vibeFi.VibeUsuario (
   grauDancavel DOUBLE PRECISION NULL,
   PRIMARY KEY (idVibe),
   CONSTRAINT fk_VibeUsuario_VibeBase
-    FOREIGN KEY (Origem)
+    FOREIGN KEY (origem)
     REFERENCES vibeFi.VibeBase (idVibe)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON DELETE SET NULL,
   CONSTRAINT fk_VibeUsuario_Usuario
     FOREIGN KEY (idUsuario)
     REFERENCES vibeFi.Usuario (idSpotify)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
+    ON DELETE CASCADE);
 CREATE UNIQUE INDEX idVibeUsuario_UNIQUE ON vibeFi.VibeUsuario (idVibe ASC);
-
-CREATE INDEX fk_VibeUsuario_VibeBase_idx ON vibeFi.VibeUsuario (Origem ASC);
-
+CREATE INDEX fk_VibeUsuario_VibeBase_idx ON vibeFi.VibeUsuario (origem ASC);
 CREATE INDEX fk_VibeUsuario_Usuario_idx ON vibeFi.VibeUsuario (idUsuario ASC);
 
 
-DROP TABLE IF EXISTS vibeFi.vibeSeed ;
-
-DROP TYPE IF EXISTS SeedType;
 CREATE TYPE SeedType AS ENUM ('track', 'artist', 'genre');
 CREATE TABLE IF NOT EXISTS vibeFi.vibeSeed (
   vibe CHAR(22) NOT NULL,
@@ -75,5 +64,4 @@ CREATE TABLE IF NOT EXISTS vibeFi.vibeSeed (
   CONSTRAINT fk_vibeSeed_VibeUsuario1
     FOREIGN KEY (vibe)
     REFERENCES vibeFi.VibeUsuario (idVibe)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE CASCADE);
