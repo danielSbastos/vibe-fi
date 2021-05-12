16efbfc2e2b836e0a64a26b9a212236d1ab143b4
Leonardo Gomide <leocgomide@gmail.com>
2021-05-12 10:29:01 -0300

    using PreparedStatements

--- back-end/src/main/java/dao/UserDAO.java
+++ back-end/src/main/java/dao/UserDAO.java
@@ -11,13 +11,18 @@ public class UserDAO {
         connection = null;
     }
 
+    /* connection methods */
     protected boolean connect() {
         boolean status = false;
-        try {
-            connection = DatabaseConnection.getConnection();
+        if (connection == null) {
+            try {
+                connection = DatabaseConnection.getConnection();
+                status = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
             status = true;
-        } catch (Exception e) {
-            e.printStackTrace();
         }
         return status;
     }
@@ -33,23 +38,93 @@ public class UserDAO {
         return status;
     }
 
+    /******************************************************************************************/
+
+    /* preparedStatement methods */
+
+    private PreparedStatement prepareCreateUserSQLStatement(User user) throws SQLException {
+        connect();
+
+        String query = "INSERT INTO vibefi.user("
+                + "idSpotify, name, popularity, tempo, valence, liveness, acousticness, danceability, energy, speechiness, instrumentalness)"
+                + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        PreparedStatement pst = connection.prepareStatement(query);
+
+        pst.setString(1, user.getId());
+        pst.setString(2, user.getName());
+        pst.setObject(3, user.getStats().getPopularity());
+        pst.setObject(4, user.getStats().getTempo());
+        pst.setObject(5, user.getStats().getValence());
+        pst.setObject(6, user.getStats().getLiveness());
+        pst.setObject(7, user.getStats().getAcousticness());
+        pst.setObject(8, user.getStats().getDanceability());
+        pst.setObject(9, user.getStats().getEnergy());
+        pst.setObject(10, user.getStats().getSpeechiness());
+        pst.setDouble(11, user.getStats().getInstrumentalness());
+
+        return pst;
+    }
+
+    private PreparedStatement prepareGetUserSQLStatement(String id) throws SQLException {
+        connect();
+
+        String query = "SELECT * FROM vibefi.user WHERE idSpotify = ?;";
+        PreparedStatement pst = connection.prepareStatement(query);
+
+        pst.setString(1, id);
+
+        return pst;
+    }
+
+    private PreparedStatement prepareUpdateUserSQLStatement(User user) throws SQLException {
+        connect();
+
+        String query = "UPDATE vibefi.user"
+                + " SET name=?, popularity=?, tempo=?, valence=?, liveness=?, acousticness=?, danceability=?, energy=?, speechiness=?, instrumentalness=?"
+                + " WHERE idSpotify=?;";
+        PreparedStatement pst = connection.prepareStatement(query);
+
+        pst.setString(1, user.getName());
+        pst.setObject(2, user.getStats().getPopularity());
+        pst.setObject(3, user.getStats().getTempo());
+        pst.setObject(4, user.getStats().getValence());
+        pst.setObject(5, user.getStats().getLiveness());
+        pst.setObject(6, user.getStats().getAcousticness());
+        pst.setObject(7, user.getStats().getDanceability());
+        pst.setObject(8, user.getStats().getEnergy());
+        pst.setObject(9, user.getStats().getSpeechiness());
+        pst.setObject(10, user.getStats().getInstrumentalness());
+        pst.setString(11, user.getId());
+
+        return pst;
+    }
+
+    private PreparedStatement prepareDeleteUserSQLStatement(String id) throws SQLException {
+        connect();
+
+        String query = "DELETE FROM vibefi.user WHERE idSpotify = ?;";
+        PreparedStatement pst = connection.prepareStatement(query);
+
+        pst.setString(1, id);
+
+        return pst;
+    }
+
+    /******************************************************************************************/
+
+    /* CRUD */
+
     public boolean createUser(User user) {
         boolean status = false;
         if (connection == null) {
             connect();
         }
 
-        String query = "INSERT INTO vibefi.user (idSpotify, name, popularity, tempo, valence, liveness, acousticness, danceability, energy, speechiness, instrumentalness)"
-                + " VALUES ('" + user.getId() + "', '" + user.getName() + "', " + user.getStats().getPopularity() + ", "
-                + user.getStats().getTempo() + ", " + user.getStats().getValence() + ", "
-                + user.getStats().getLiveness() + ", " + user.getStats().getAcousticness() + ", "
-                + user.getStats().getDanceability() + ", " + user.getStats().getEnergy() + ", "
-                + user.getStats().getSpeechiness() + ", " + user.getStats().getInstrumentalness() + ");";
-
         try {
-            Statement st = connection.createStatement();
-            st.executeUpdate(query);
-            st.close();
+            PreparedStatement pst = prepareCreateUserSQLStatement(user);
+
+            pst.executeUpdate();
+            pst.close();
             status = true;
         } catch (SQLException u) {
             throw new RuntimeException(u);
@@ -64,15 +139,17 @@ public class UserDAO {
         }
 
         try {
-            Statement st = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            ResultSet rs = st.executeQuery("SELECT * FROM vibefi.user WHERE idSpotify = '" + id + "';");
+            PreparedStatement pst = prepareGetUserSQLStatement(id);
+            ResultSet rs = pst.executeQuery();
+
             if (rs.next()) {
-                user = new User(rs.getString("idSpotify"), rs.getString("name"), rs.getInt("popularity"),
-                        rs.getDouble("tempo"), rs.getDouble("valence"), rs.getDouble("liveness"),
-                        rs.getDouble("acousticness"), rs.getDouble("danceability"), rs.getDouble("energy"),
-                        rs.getDouble("speechiness"), rs.getDouble("instrumentalness"));
+                user = new User(rs.getString("idSpotify"), rs.getString("name"), (Integer) rs.getObject("popularity"),
+                        (Double) rs.getObject("tempo"), (Double) rs.getObject("valence"),
+                        (Double) rs.getObject("liveness"), (Double) rs.getObject("acousticness"),
+                        (Double) rs.getObject("danceability"), (Double) rs.getObject("energy"),
+                        (Double) rs.getObject("speechiness"), (Double) rs.getObject("instrumentalness"));
             }
-            st.close();
+            pst.close();
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
@@ -85,18 +162,10 @@ public class UserDAO {
             connect();
         }
 
-        String query = "UPDATE vibefi.user" + " SET name = '" + user.getName() + "', popularity = "
-                + user.getStats().getPopularity() + ", tempo = " + user.getStats().getTempo() + ", valence = "
-                + user.getStats().getValence() + ", liveness = " + user.getStats().getLiveness() + ", acousticness = "
-                + user.getStats().getAcousticness() + ", danceability = " + user.getStats().getDanceability()
-                + ", energy = " + user.getStats().getEnergy() + ", speechiness = " + user.getStats().getSpeechiness()
-                + ", instrumentalness = " + user.getStats().getInstrumentalness() + " WHERE idSpotify = '"
-                + user.getId() + "';";
-
         try {
-            Statement st = connection.createStatement();
-            st.executeUpdate(query);
-            st.close();
+            PreparedStatement pst = prepareUpdateUserSQLStatement(user);
+            pst.executeUpdate();
+            pst.close();
             status = true;
         } catch (SQLException u) {
             throw new RuntimeException(u);
@@ -107,16 +176,11 @@ public class UserDAO {
 
     public boolean deleteUser(String id) {
         boolean status = false;
-        if (connection == null) {
-            connect();
-        }
-
-        String query = "DELETE FROM vibefi.user WHERE idSpotify = '" + id + "';";
 
         try {
-            Statement st = connection.createStatement();
-            st.executeUpdate(query);
-            st.close();
+            PreparedStatement pst = prepareDeleteUserSQLStatement(id);
+            pst.executeUpdate();
+            pst.close();
             status = true;
         } catch (SQLException u) {
             throw new RuntimeException(u);
