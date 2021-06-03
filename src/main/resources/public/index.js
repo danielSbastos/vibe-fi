da927f0260d504bb8355caf17b350c3886127176
Leonardo Gomide <leocgomide@gmail.com>
2021-06-03 19:13:07 -0300

    remove vibe pics

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -1,29 +1,28 @@
 window.onload = () => {
-    configureLoginUrl();
-    alterLoginText();
+	configureLoginUrl()
+	alterLoginText()
 
-    playlist = [];
-    const userId = getCookie("user_id");
+	playlist = []
+	const userId = getCookie("user_id")
 
-    $.ajax({
-        url: `${window.location.protocol}//${window.location.host}/vibe/user/${userId}`,
-        type: "GET",
-        dataType: "json",
-    })
-    .done(function (data) {
-         playlist = data;
-         loadhome(data);
-    })
-    .fail(function (xhr, status, error) {
-	       console.log(error);
-           setNoCards();
-    });
+	$.ajax({
+		url: `${window.location.protocol}//${window.location.host}/vibe/user/${userId}`,
+		type: "GET",
+		dataType: "json"
+	})
+		.done(function (data) {
+			playlist = data
+		})
+		.fail(function (xhr, status, error) {
+			console.log(error)
+			setNoCards()
+		})
 }
 
-function loadhome (data){
-    cards="";
-    for  (i = 0;i<data.length;i++){
-        cards += `<div id="card" class="col-lg-4">
+function loadhome(data) {
+	cards = ""
+	for (i = 0; i < data.length; i++) {
+		cards += `<div id="card" class="col-lg-4">
         <div class="card border-dark w100 vibecard">
             <img class="card-img-top" src="${data[i].imgUrl}" alt="Imagem de capa do card">
             <div class="card-body">
@@ -31,40 +30,43 @@ function loadhome (data){
                 <p class="card-text">${data[i].description}</p>
                 <a href="screens/vibe/?vibeId=${data[i].id}" class="btn btn-primary">Visitar</a>
             </div>
-        </div></div>`;
-    }
-    document.getElementById("divprinc").innerHTML = cards;
-
+        </div></div>`
+	}
+	document.getElementById("divprinc").innerHTML = cards
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+	var name = cname + "="
+	var decodedCookie = decodeURIComponent(document.cookie)
+	var ca = decodedCookie.split(";")
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i]
+		while (c.charAt(0) == " ") {
+			c = c.substring(1)
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length)
+		}
+	}
+	return ""
 }
 
 function alterLoginText() {
-    if (getCookie("user_id") !== "") {
-        document.getElementById("login").innerText = "Visitar perfil";
-        document.getElementById("login").href = `http://${window.location.host}/screens/perfil/`;
-    }
+	if (getCookie("user_id") !== "") {
+		document.getElementById("login").innerText = "Visitar perfil"
+		document.getElementById(
+			"login"
+		).href = `http://${window.location.host}/screens/perfil/`
+	}
 }
 
 function configureLoginUrl() {
-    document.getElementById("login").href = `${window.location.protocol}//${window.location.host}/login`;
+	document.getElementById(
+		"login"
+	).href = `${window.location.protocol}//${window.location.host}/login`
 }
 
-function setNoCards(){
-    content = `<div class="display-6 text-center p-5">Parece que você ainda não possui nenhuma playlist :(</div>`
-    $('#divprinc').html(content);
+function setNoCards() {
+	content = `<div class="display-6 text-center p-5">Parece que você ainda não possui nenhuma playlist :(</div>`
+	$("#divprinc").html(content)
 }
--- src/main/resources/public/screens/perfil/index.js
+++ src/main/resources/public/screens/perfil/index.js
@@ -134,7 +134,6 @@ function setCards(data) {
                     <div class="card vibecard">
                         <div class="card-body">
                             <h5 class="card-img-top">
-                                <img src="${e.imgUrl}" alt="">${e.name}
                                 ${
 									isSeedless
 										? '<span class="badge bg-warning text-dark">sem músicas</span>'
