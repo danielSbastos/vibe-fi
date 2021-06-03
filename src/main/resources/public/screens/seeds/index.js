ca4aa87ed0d389383c529c2fbe4b0dd47829f574
Henrique <henrique.castros@outlook.com>
2021-06-02 23:12:39 -0300

    add select seeds

---
+++ src/main/resources/public/screens/seeds/index.js
@@ -0,0 +1,134 @@
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function search(input, type) {
+  $.ajax({
+    url: `https://api.spotify.com/v1/search?q=${input}&type=${type}&limit=6`,
+    type: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + getCookie("access_token"),
+    },
+  })
+    .done(function (data) {
+      if (type == "artist") {
+        genArtists(data, type);
+      } else {
+        genTrack(data, type);
+      }
+    })
+    .fail(() => {
+      console.log("fail");
+    });
+}
+
+function removeItem(obj, type) {
+  let fullId = obj.innerText.trim() + ";" + obj.id + ";" + type;
+  let element = document.getElementById(fullId);
+  if (element) {
+    element.firstElementChild.classList.toggle("cardActive");
+  }
+  $(`#${obj.id}`).remove();
+  let count = $(`#artistList`).attr(type);
+  $(`#artistList`).attr(`${type}`, parseInt(count) - 1);
+}
+
+function toggleActive(object) {
+  let info = object.id.split(";");
+  let count = $(`#artistList`).attr(info[2]);
+  let icon = info[2] == "track" ? "fas fa-music" : "fas fa-user-alt";
+
+  if ($(`#${info[1]}`).length) {
+    $(`#${info[1]}`).remove();
+    $(`#artistList`).attr(`${info[2]}`, parseInt(count) - 1);
+    object.firstElementChild.classList.toggle("cardActive");
+  } else {
+    if (count >= 5) {
+      alert(`O máximo de ${info[2]} foi atingido`);
+    } else {
+      $("#artistList").append(
+        `<li class="list-group-item bg-transparent text-light border-0" id="${info[1]}" seedType="${info[2]}">
+            <i class="${icon}"></i>
+            ${info[0]} 
+            <i class="fas fa-trash float-end" onclick="removeItem(this.parentNode, '${info[2]}')"></i>
+        </li>`
+      );
+      $(`#artistList`).attr(`${info[2]}`, parseInt(count) + 1);
+
+      object.firstElementChild.classList.toggle("cardActive");
+    }
+  }
+}
+
+function genArtists(data, type) {
+  let content = "";
+  data.artists.items.forEach((e) => {
+    let url = "";
+    if (e.images.length) {
+      url = e.images[0].url;
+    } else {
+      url = "../../assets/VibefiFull.png";
+    }
+    content += `<div class="col-md-4 my-3" onclick="toggleActive(this)" id="${e.name};${e.id};${type}">
+            <div class="card bg-dark text-white" style="max-height: 260px; overflow: hidden;">
+                <img src="${url}" class="card-img" alt="..." />
+                <div class="card-img-overlay">
+                    <h5 class="card-title artName float-end">${e.name}</h5>
+                </div>
+                <div class="clear-fix"></div>
+            </div>
+        </div>`;
+  });
+  $("#cardsSelection").html(content);
+}
+
+function genTrack(data, type) {
+  let content = "";
+  data.tracks.items.forEach((e) => {
+    let url = "";
+    if (e.album.images.length) {
+      url = e.album.images[0].url;
+    } else {
+      url = "../../assets/VibefiFull.png";
+    }
+    content += `<div class="col-md-4 my-3" onclick="toggleActive(this)" id="${e.name};${e.id};${type}">
+              <div class="card bg-dark text-white" style="max-height: 260px; overflow: hidden;">
+                  <img src="${url}" class="card-img" alt="..." />
+                  <div class="card-img-overlay">
+                      <h5 class="card-title artName float-end">${e.name}</h5>
+                  </div>
+                  <div class="clear-fix"></div>
+              </div>
+          </div>`;
+  });
+
+  $("#cardsSelection").html(content);
+}
+
+function sendIDS(){
+  let ids = [];
+  let children = $('#artistList').children();
+  for(var i = 0; i < children.length; i++){
+    ids[i] = [children[i].id, children[i].getAttribute('seedType')];
+  }
+  console.log(ids);
+}
+
+function prepSearch() {
+  let input = $("#inputSearch").val();
+  let option = $("#selectSearch").val() === "Artista" ? "artist" : "track";
+  search(input, option);
+}
